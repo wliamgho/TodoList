fa8063cba1d8b06e0863df76f03edb3b3452130d
William <will.liiam06@gmail.com>
2019-11-18 14:59:59 +0700

    feat: add core data manager and entity

---
+++ TodoList/Classes/Entity/Entity.swift
@@ -0,0 +1,24 @@
+//
+//  Entity.swift
+//  TodoList
+//
+//  Created by William on 18/11/19.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+protocol Entity {
+  static func entityName() -> String
+
+  static func parse(json: [String: Any], context: NSManagedObjectContext) -> Entity?
+
+  func same(entity: Entity?) -> Bool
+
+  static func newEntity(context: NSManagedObjectContext) -> Entity?
+
+  func update(json: [String: Any]) -> Entity
+
+  func toJson() -> [String: Any]
+}
