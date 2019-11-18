9463720f1775d07015de26f0311245eac7f861b4
William <will.liiam06@gmail.com>
2019-11-18 15:08:25 +0700

    feat: add mock persistent container

---
+++ TodoListTests/Mock/PersistentContainer/MockPersistentContainer.swift
@@ -0,0 +1,27 @@
+//
+//  MockPersistentContainer.swift
+//  TodoListTests
+//
+//  Created by William on 18/11/19.
+//  Copyright © 2019 William. All rights reserved.
+//
+
+import CoreData
+
+class MockPersistentContainer {
+  lazy var container: NSPersistentContainer = {
+      let container = NSPersistentContainer(name: "TodoList")
+      let description = NSPersistentStoreDescription()
+      description.type = NSInMemoryStoreType
+      description.shouldAddStoreAsynchronously = false
+
+      container.persistentStoreDescriptions = [description]
+      container.loadPersistentStores(completionHandler: { (description, error) in
+        precondition(description.type == NSInMemoryStoreType)
+        if let error = error {
+          fatalError("Create an in-memory coordinator failed \(error)")
+        }
+      })
+      return container
+  }()
+}
